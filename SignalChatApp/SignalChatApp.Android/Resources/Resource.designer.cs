99fe912fbad42077fb9c43a707ed3420568759fb
codingfab <codingfab@users.noreply.github.com>
2020-05-03 20:36:56 +0200

    Custom skin

--- SignalChatApp/SignalChatApp.Android/Resources/Resource.designer.cs
+++ SignalChatApp/SignalChatApp.Android/Resources/Resource.designer.cs
@@ -15,7 +15,7 @@ namespace SignalChatApp.Droid
 {
 	
 	
-	[System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
 	public partial class Resource
 	{
 		
@@ -5789,10 +5789,13 @@ namespace SignalChatApp.Droid
 			public const int notify_panel_notification_icon_bg = 2131165299;
 			
 			// aapt resource value: 0x7F070074
-			public const int tooltip_frame_dark = 2131165300;
+			public const int selfie = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int tooltip_frame_light = 2131165301;
+			public const int tooltip_frame_dark = 2131165301;
+			
+			// aapt resource value: 0x7F070076
+			public const int tooltip_frame_light = 2131165302;
 			
 			static Drawable()
 			{
